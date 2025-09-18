8a780e4a21ef4a74e01920e9efefda07bfea92c7
muhammadriyansadiq <riyan@digitechinfra.com>
2025-09-18 12:59:14 +0500

    Initial commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,38 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import Home from "@/pages/Home";
+import DashboardLayout from "@/components/layouts/dashboard-layout";
+import DashboardPage from "@/pages/Home";
 
 // import AddressAutocomplete from "@/pages/location";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    index: true,
-  },
-  {
+  // {
+  //   path: "/",
+  //   element: <Home />,
+  //   index: true,
+  // },
+  // {
     // element: <ProtectedRoute roleAllowed={["Admin", "Franchise"]} />,
     // children: [
       
      
      
     // ],
-  },
+  // },
+  {
+children: [
+      {
+        element: <DashboardLayout />,
+        children: [
+          { path: "/", element: <Home /> },
 
-
+          // { path: "/franchise-list", element: <FranchiseList /> },
+          // { path: "/lead-list", element: <LeadList /> },
+        ],
+      },
+    ],
+}
   // Catch-all for 404 errors
   
 ]);
